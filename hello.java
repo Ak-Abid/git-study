2f4502a4328a46d0c0a0d5c0617a402606b10635
akabid <akhtaruzzamanabid@gmail.com>
2020-06-14 10:16:16 -0700

    Added a new method called display to Hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
 public class hello{
     public static void main(string[] args) {
         system.out.println("Vs code os Awesome...");
+
+        greeting("AK Abid");
+    }
+
+    public static void greeting(String[] name) {
+        system.out.println("Welcome " + name);
     }
 }
+
